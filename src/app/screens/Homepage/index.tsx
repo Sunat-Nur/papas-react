45035f0bbd58ecd59a9c8d216f2715bf74fdedd9
Sunat_nur <uzbekistan9699@gmail.com>
2023-11-15 12:14:20 +0900

    :feat: homepage topRestaurant section development

--- src/app/screens/Homepage/index.tsx
+++ src/app/screens/Homepage/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import {Container} from "@mui/material";
+import {BestRestaurants} from "./bestRestaurants";
+import {TopRestaurants} from "./topRestaurants";
+import {BestDishes} from "./bestDishes";
+import {Advertisements} from "./advertisements";
+import {Statistics} from "./statistics";
+import {Events} from "./events";
+import {Recommendations} from "./recommendations";
+import "../../../css/home.css"
+
 
 export function Homepage() {
-    return <Container>Homepage</Container>;
+    return <div className="homepage">
+        <Statistics/>
+        <TopRestaurants/>
+        <BestRestaurants/>
+        <BestDishes/>
+        <Advertisements/>
+        <Events/>
+        <Recommendations/>
+    </div>;
 }
+
