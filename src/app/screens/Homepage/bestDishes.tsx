bec3507447ef48a87c88dd35d13e213bad270a66
Sunat_nur <uzbekistan9699@gmail.com>
2023-11-18 13:08:00 +0900

    feat: develop nester routing for restaurant screen

--- src/app/screens/Homepage/bestDishes.tsx
+++ src/app/screens/Homepage/bestDishes.tsx
@@ -1,10 +1,126 @@
-import {Container} from "@mui/material";
+import {Box, Container, Stack,} from "@mui/material";
+import {MonetizationOn} from "@mui/icons-material";
 import React from "react";
 
-export function  BestDishes() {
+export function BestDishes() {
     return (
-        <div className="top_restaurant_frame">
-            <Container>BestDishes</Container>
+        <div className="best_dishes_frame">
+            <Container>
+                <Stack flexDirection={"column"} alignItems={"center"}>
+                    <Box className="category_title"> Trendagi ovqatlar</Box>
+                    <Stack sx={{mt: "43px"}} flexDirection={"row"}>
+
+                        <Box className={"dish_box"}>
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage: `url(
+                                    https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg                                   
+                                    )`
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    <div> Batafsil ko'rish</div>
+                                    <img
+                                        src={"/icons/Arrow8.svg"}
+                                        style={{marginLeft: "9px"}}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>Cheese Pasta</span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn/>
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                        <Box className={"dish_box"}>
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage: `url(
+                                    https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg                                   
+                                    )`
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    <div> Batafsil ko'rish</div>
+                                    <img
+                                        src={"/icons/Arrow8.svg"}
+                                        style={{marginLeft: "9px"}}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>Cheese Pasta</span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn/>
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                        <Box className={"dish_box"}>
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage: `url(
+                                    https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg                                   
+                                    )`
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    <div> Batafsil ko'rish</div>
+                                    <img
+                                        src={"/icons/Arrow8.svg"}
+                                        style={{marginLeft: "9px"}}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>Cheese Pasta</span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn/>
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                        <Box className={"dish_box"}>
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage: `url(
+                                    https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg                                   
+                                    )`
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    <div> Batafsil ko'rish</div>
+                                    <img
+                                        src={"/icons/Arrow8.svg"}
+                                        style={{marginLeft: "9px"}}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>Cheese Pasta</span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn/>
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                    </Stack>
+                </Stack>
+            </Container>
         </div>
     );
 }
--- src/app/screens/Homepage/events.tsx
+++ src/app/screens/Homepage/events.tsx
@@ -1,10 +1,132 @@
-import {Container} from "@mui/material";
 import React from "react";
+import {Box, Container, Stack} from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore, { Autoplay, Navigation, Pagination} from 'swiper/react';
+// SwiperCore.use([Autoplay, Navigation, Pagination]);
 
-export function  Events() {
+
+
+export function Events() {
+    const events_list = [
+        {
+            title: "Boyin foodga marhamat",
+            desc: "yangicha uslubda yangicha tam va yangicha his",
+            author: "sunat_nur",
+            date: "2023.11.16",
+            location: "tashkent, nurafshon ko'cha",
+            img:"/restaurant/sale_pizza.jpeg",
+        },
+        {
+            title: "Boyin foodga marhamat",
+            desc: "yangicha uslubda yangicha tam va yangicha his",
+            author: "sunat_nur",
+            date: "2023.11.16",
+            location: "tashkent, nurafshon ko'cha",
+            img: "/restaurant/sale_pizza.jpeg",
+        },
+        {
+            title: "Boyin foodga marhamat",
+            desc: "yangicha uslubda yangicha tam va yangicha his",
+            author: "sunat_nur",
+            date: "2023.11.16",
+            location: "tashkent, nurafshon ko'cha",
+            img: "/restaurant/sale_pizza.jpeg",
+        },
+        {
+            title: "Boyin foodga marhamat",
+            desc: "yangicha uslubda yangicha tam va yangicha his",
+            author: "sunat_nur",
+            date: "2023.11.16",
+            location: "tashkent, nurafshon ko'cha",
+            img: "/restaurant/sale_pizza.jpeg",
+        },
+    ];
+
+
     return (
-        <div className="top_restaurant_frame">
-            <Container>Events</Container>
+        <div className={"events_frame"}>
+            <Container sx={{ overflow: "hidden"}}>
+                <Stack className={"events_main"}>
+                    <Box className={"events_text"}>
+                        <span className={"category_title"}>Hodisalar</span>
+                    </Box>
+                    <Box className={"prev_next_frame"}>
+                        <img
+                            src={"/icons/slider_page.svg"}
+                            className={"swiper-button-prev"}
+                        />
+                        <div className={"dot_frame_pagination swiper-pagination"}></div>
+                    </Box>
+                    {/*<Swiper*/}
+                    {/*    className={"events_info swiper-wrapper"}*/}
+                    {/*    slidesPerView={"auto"}*/}
+                    {/*    centeredSlides={true}*/}
+                    {/*    spaceBetween={30}*/}
+                    {/*    navigation={{*/}
+                    {/*        nextEl: ".swiper-button-next",*/}
+                    {/*        prevEl: ".swiper-button-prev",*/}
+                    {/*    }}*/}
+                    {/*    pagination={{*/}
+                    {/*        el: ".swiper-pagination",*/}
+                    {/*        clickable: true,*/}
+                    {/*    }}*/}
+                    {/*    autoplay={{*/}
+                    {/*        delay: 2000,*/}
+                    {/*        disableOnInteraction: true,*/}
+                    {/*    }}*/}
+                    {/*>*/}
+                    {/*    {events_list.map((value, number) => {*/}
+                    {/*        return (*/}
+                    {/*            <SwiperSlide className={"events_info_frame"}>*/}
+                    {/*                <div className={"events_img"}>*/}
+                    {/*                    <img src={value.img} className={"events_img"} />*/}
+                    {/*                </div>*/}
+                    {/*                <Box className={"events_desc"}>*/}
+                    {/*                    <Box className={"events_bott"}>*/}
+                    {/*                        <Box className={"bott_left"}>*/}
+                    {/*                            <div className={"event_title_speaker"}>*/}
+                    {/*                                <strong>{value.title}</strong>*/}
+                    {/*                                <div className={"event_organizator"}>*/}
+                    {/*                                    <img*/}
+                    {/*                                        src={"/icons/men.svg"}*/}
+                    {/*                                        style={{ width: "20px", marginRight: "10px" }}*/}
+                    {/*                                    />*/}
+                    {/*                                    <p className={"spec_text_author"}>{value["author"]}</p>*/}
+                    {/*                                </div>*/}
+                    {/*                            </div>*/}
+                    {/*                            <p className={"text_desc"}*/}
+                    {/*                               style={{ marginTop: "10px" }}*/}
+                    {/*                            >*/}
+                    {/*                                {" "}*/}
+                    {/*                                {value.desc}{" "}*/}
+                    {/*                            </p>*/}
+                    {/*                            <div className={"bott_info"}*/}
+                    {/*                                 style={{ marginTop: "10px" }}*/}
+                    {/*                            >*/}
+                    {/*                                <div className={"bott_info_main"}>*/}
+                    {/*                                    <img*/}
+                    {/*                                        src={"/icons/Calendar.png"}*/}
+                    {/*                                        style={{ marginRight: "10px" }}*/}
+                    {/*                                    />*/}
+                    {/*                                    {value.date}*/}
+                    {/*                                </div>*/}
+                    {/*                                <div className={"bott_info_main"}>*/}
+                    {/*                                    <img*/}
+                    {/*                                        src={"/icons/map-marked.svg"}*/}
+                    {/*                                        style={{ marginRight: "10px" }}*/}
+                    {/*                                    />*/}
+                    {/*                                    {value.location}*/}
+                    {/*                                </div>*/}
+                    {/*                            </div>*/}
+                    {/*                        </Box>*/}
+                    {/*                    </Box>*/}
+                    {/*                </Box>*/}
+                    {/*            </SwiperSlide>*/}
+                    {/*        );*/}
+                    {/*    })}*/}
+                    {/*</Swiper>*/}
+                </Stack>
+            </Container>
         </div>
     );
 }
