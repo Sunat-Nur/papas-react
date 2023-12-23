3948c19de90627a330894c89feaa00e05201b7ea
Sunat_nur <uzbekistan9699@gmail.com>
2023-12-24 03:14:07 +0900

    feat: retrive  bestDishes datta

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+import HomePageReducer from './screens/Homepage/slice';
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(reduxLogger),
   reducer: {
+    homePage: HomePageReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
 >;
