7470eebff5fb0b51b77e6ba757c1e3b1e950e1fa
Sunat_nur <uzbekistan9699@gmail.com>
2023-12-26 16:50:15 +0900

    feat: develop signup  process

--- src/app/apiServices/memberApiService.ts
+++ src/app/apiServices/memberApiService.ts
@@ -29,7 +29,27 @@ class MemberApiService {
         } catch (err: any) {
             console.log(`ERROR ::: loginRequest ${err.message}`);
         }
-    }
+    };
+
+    public async signupRequest(signup_data: any) {
+        try {
+            const result = await axios.post(this.path + "/signup", signup_data, //  login_data ni request_body ga beryabman
+                {withCredentials: true});
+            console.log("state:", result.data.state);
+            assert.ok(result?.data, Definer.general_err1);
+            assert.ok(result?.data?.state !== "fail", result?.data.message);
+
+            // member interface orqali hosil qilib olyabman
+            const member: Member = result.data.data;
+
+            // localStorage dan member_data objectini hosil qilib member obj ni jsonga ugirib saqla deyabman
+            localStorage.setItem("member_data", JSON.stringify(member));
+            return member;
+        } catch (err: any) {
+            console.log(`ERROR ::: signupRequest ${err.message}`);
+        }
+    };
+
 
 
 }
